f64f938989ae9e48085d9e71e5f5e9b33f5ef3b0
Agam Mundra <agam@Agams-MacBook-Air.local>
2026-07-10 21:39:52 +0530

    Added prediction UI

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 import streamlit as st
 import tensorflow as tf
+import numpy as np
 
-st.title("RadioML Test")
+st.set_page_config(page_title="RadioML Classifier")
 
-st.write("TensorFlow version:", tf.__version__)
+st.title("📡 RadioML Automatic Modulation Classifier")
 
-st.write("Before loading model...")
+model = tf.keras.models.load_model("radioml_week4_final_model.keras")
 
-try:
-    model = tf.keras.models.load_model("radioml_week4_final_model.keras")
-    st.success("✅ Model loaded successfully!")
-except Exception as e:
-    st.error(e)
+st.success("Model loaded successfully!")
+
+uploaded_file = st.file_uploader(
+    "Upload a .npy IQ signal",
+    type=["npy"]
+)
+
+if uploaded_file is not None:
+
+    signal = np.load(uploaded_file)
+
+    st.write("Original shape:", signal.shape)
+
+    if signal.shape == (2,128):
+        signal = signal.reshape(1,2,128,1)
+
+    prediction = model.predict(signal)
+
+    predicted_class = np.argmax(prediction)
+
+    confidence = np.max(prediction)
+
+    st.write("Predicted class:", predicted_class)
+
+    st.write(f"Confidence: {confidence*100:.2f}%")
